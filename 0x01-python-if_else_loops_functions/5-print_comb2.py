5d0bd10e38606c5909ee829a26aac99cf6f9f45d
Kadiri Dauda <Kadiri.dauda@gmail.com>
2022-11-29 09:14:40 -0800

    made changes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
+for i in range(0, 100):
     if i == 99:
         print(f"{i}")
-    print("{:02}".format(i), end=", ")
+    else:
+        print("{:02}".format(i), end=", ")
