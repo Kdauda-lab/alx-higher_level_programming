54271ad15f19e703024c5a45d3369a8a445420e1
Kadiri Dauda <Kadiri.dauda@gmail.com>
2022-11-29 05:32:47 -0800

    A program to produce the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lst_dig = number%10
+if lst_dig > 5:
+    print(f"Last digit of {number} is {lst_dig} and is greater than 5")
+elif lst_dig == 0:
+    print(f"Last digit of {number} is {lst_dig} and is 0")
+elif lst_dig < 6 and lst_dig != 0:
+    print(f"Last digit of {number} is {lst_dig} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,1 +1,2 @@
 0-positive_or_negative.py - A program which test if a number is positive, negative or zero
+ 1-last_digit.py - A program to produce the last digit of a number and test if the last digit is greater than 5, equal to 0, or less than 6 and not equal to 0
